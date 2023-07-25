5063d7bb46d2574e773c017a09c45683f12a8526
David Vojvodić <davidvojvodic1@gmail.com>
2023-07-25 21:35:07 +0200

    api limit for conversation

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,8 +2,6 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 
-import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -12,14 +10,16 @@ const openai = new OpenAIApi(configuration);
 
 const defaultContextMessage = {
     role: 'system',
-    content: "You are an assistant fluent in Slovenian. Answer the following as if you were speaking to a Slovenian user:"
+    content: "Generate an image that represents the following scene: [describe the scene you want to be generated in Slovenian language]."
 };
+
+
 
 export async function POST(req: Request) {
     try {
         const {userId} = auth();
         const body = await req.json();
-        let {messages} = body;
+        let {prompt, amount = 1, resolution = "512x512"} = body;
 
         if(!userId) {
             return new NextResponse("Unauthorized", {status: 401});
@@ -29,30 +29,30 @@ export async function POST(req: Request) {
             return new NextResponse("OpenAI API key not configured", {status: 500});
         }
 
-        if(!messages) {
-            return new NextResponse("Messages are required", {status: 400});
+        if(!prompt) {
+            return new NextResponse("Prompt is required", {status: 400});
         }
-
-        const freeTrial = await checkApiLimit()
-
-        if(!freeTrial) {
-            return new NextResponse("Free trial limit reached", {status: 403});
+        if(!amount) {
+            return new NextResponse("Amount is required", {status: 400});
+        }
+        if(!resolution) {
+            return new NextResponse("Resolution is required", {status: 400});
         }
 
         // Prepend the default context message to the messages
-        messages = [defaultContextMessage, ...messages];
+        // prompt = [defaultContextMessage, ...prompt];
 
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: parseInt(amount, 10),
+            size: resolution,
+            
         });
 
-        await increaseApiLimit()
-
-        return NextResponse.json(response.data.choices[0].message);
+        return NextResponse.json(response.data.data);
 
     } catch (error) {
-        console.log("[CONVERSATION_ERROR]", error);
+        console.log("[IMAGE_ERROR]", error);
         return new NextResponse("Internal error", {status: 500});
     }
 }
